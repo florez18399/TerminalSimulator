b12c371e7f0327afb9a666961b2cd6557c5b2b63
Andres Florez <andres.florez03@uptc.edu.co>
2018-04-16 13:18:06 -0500

    Creación de la clase Passenger

---
+++ src/models/Passenger.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Passenger {
+	private String name;
+	private int id;
+	private Destiny destiny;
+
+	public Passenger(String name, int id, Destiny destiny) {
+		this.name = name;
+		this.destiny = destiny;
+	}
+
+	public Passenger(Destiny destiny) {
+		super();
+		this.destiny = destiny;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Destiny getDestiny() {
+		return destiny;
+	}
+
+	public void setDestiny(Destiny destiny) {
+		this.destiny = destiny;
+	}
+
+}
