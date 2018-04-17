965effb576493a9bf347c4a18500c77b9a45571e
Andres Florez <andres.florez03@uptc.edu.co>
2018-04-16 22:34:09 -0500

    Creada la estructura de la clase FileManager

--- src/persistence/FileManager.java
+++ src/persistence/FileManager.java
@@ -1,5 +1,26 @@
 package persistence;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class FileManager {
+	private File file;
+	private FileWriter fileWriter;
+	private FileReader fileReader;
+	private BufferedWriter bufferedWriter;
+	private BufferedReader bufferedReader;
+	private OpenForm openForm;
+
+	public FileManager(String nameFile, OpenForm openForm) {
+		file = new File(nameFile);
+		this.openForm = openForm;
+	}
+
+	public void setOpenForm(OpenForm openFor) {
+		this.openForm = openFor;
+	}
 
 }
