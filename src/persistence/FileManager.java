c9f5719269eb145d12f121a2291f24f09beb9483
Andres Florez <andres.florez03@uptc.edu.co>
2018-04-16 22:39:40 -0500

    Método de abrir archivo creado en la clase FileManager

--- src/persistence/FileManager.java
+++ src/persistence/FileManager.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileManager {
 	private File file;
@@ -17,6 +18,29 @@ public class FileManager {
 	public FileManager(String nameFile, OpenForm openForm) {
 		file = new File(nameFile);
 		this.openForm = openForm;
+	}
+
+	public void open() {
+		try {
+			switch (this.openForm) {
+			case WRITE:
+				fileWriter = new FileWriter(file);
+				bufferedWriter = new BufferedWriter(fileWriter);
+				break;
+			case READ:
+				fileReader = new FileReader(file);
+				bufferedReader = new BufferedReader(fileReader);
+				break;
+			case APPEND:
+				fileWriter = new FileWriter(file, true);
+				bufferedWriter = new BufferedWriter(fileWriter);
+				break;
+			default:
+				break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void setOpenForm(OpenForm openFor) {
